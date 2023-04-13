f308936ca5f64601eb91bc6137536efebb46eb42
rodrigo martinez duvivier <rodrigo.martinez224@gmail.com>
2023-04-12 21:10:40 -0300

    v7

--- src/asyncMock.js
+++ src/asyncMock.js
@@ -4,7 +4,7 @@ const products = [
         id: '1',
         name:'Iphone',
         price: 1000,
-        Image:'../Img/img-productos02',
+        Image: "/Img/img-producto01.jpg",
         category: 'celular',
         stock: 25,
     
@@ -12,7 +12,7 @@ const products = [
     {
         id: '2',
         name:'Samsungs22',
-        Image:'../Img/img-productos01',
+        Image: "/Img/img-producto02.jpg",
         price: 2000,
         category: 'celular',
         stock: 15,
@@ -21,7 +21,7 @@ const products = [
     {
         id: '3',
         name:'nokia1100',
-        Image:'../Img/img-productos03',
+        Image: "/Img/img-producto03.jpg",
         price: 3000,
         category: 'celular',
         stock: 10,
